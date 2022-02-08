064cd65cd6e6c99e4b7d0c195e3f9434a2ce0cd7
mona-98 <b.mohana1998@gmail.com>
2022-02-08 13:51:03 +0530

    octual to binary conversion

--- module/src/code/basicprogram/Program62.java
+++ module/src/code/basicprogram/Program62.java
@@ -6,9 +6,44 @@ public class Program62 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         long binaryNumber = scan.nextInt();
-        binaryToOctal(binaryNumber);
+        long octOutput=binaryToOctal(binaryNumber);
+        long octalNumber = scan.nextLong();
+        long binOutput = octalToBinary(octalNumber);
+        System.out.println(octOutput);
+        System.out.println(binOutput);
     }
-    public static int binaryToOctal(long n) {
-        return 0 ;
+    public static long binaryToOctal(long n) {
+        int oct = 0, dec = 0, i = 0;
+
+        while (n != 0) {
+            dec += (n % 10) * Math.pow(2, i);
+            ++i;
+            n /= 10;
+        }
+        i = 1;
+        while (dec != 0) {
+            oct += (dec % 8) * i;
+            dec /= 8;
+            i *= 10;
+        }
+        return oct;
+
+    }
+
+    public static long octalToBinary(long n) {
+        int dec = 0, i = 0;
+         long bin = 0;
+        while (n != 0) {
+            dec += (n % 10) * Math.pow(8, i);
+            ++i;
+            n /= 10;
+        }
+        i = 1;
+        while (dec != 0) {
+            bin += (dec % 2) * i;
+            dec /= 2;
+            i *= 10;
+        }
+        return bin;
     }
 }
